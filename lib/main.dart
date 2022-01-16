3e065a3092f93b3c8c389eaf1131094474c137e0
vaibhav0311 <sharma.vaibhav0311@gmail.com>
2022-01-16 21:52:43 +0530

    app created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:destini/story_brain.dart';
+import 'story_brain.dart';
 import 'package:flutter/material.dart';
+// import '';
 
 void main() {
   runApp(MyApp());
@@ -42,7 +43,6 @@ class _DestinyAppState extends State<DestinyApp> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
                     storybrain.getStory(),
                     style: TextStyle(
                       fontSize: 25.0,
@@ -52,19 +52,23 @@ class _DestinyAppState extends State<DestinyApp> {
               ),
               Expanded(
                 flex: 2,
-                child: TextButton(
-                  onPressed: () {
-                    //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
-                  },
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.red),
-                  ),
-                  child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    storybrain.getChoice1(),
-                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+                child: Visibility(
+                  visible: storybrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    onPressed: () {
+                      //Choice 1 made by user.
+                      setState(() {
+                        storybrain.nextStory(1);
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red),
+                    ),
+                    child: Text(
+                      storybrain.getChoice1(),
+                      style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    ),
                   ),
                 ),
               ),
@@ -73,21 +77,23 @@ class _DestinyAppState extends State<DestinyApp> {
               ),
               Expanded(
                 flex: 2,
-                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: TextButton(
-                  onPressed: () {
-                    //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-                  },
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.red),
-                  ),
-                  child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    storybrain.getChoice2(),
-                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+                child: Visibility(
+                  visible: storybrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      setState(() {
+                        storybrain.nextStory(2);
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red),
+                    ),
+                    child: Text(
+                      storybrain.getChoice2(),
+                      style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    ),
                   ),
                 ),
               ),
